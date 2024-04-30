9b4fb4518712d74b4a59303d3076752cc53ceeab
Adam Church <achurch0607@gmail.com>
2024-04-30 08:57:29 -0600

    remove blank defaults

--- src/awsSimpleName.tf
+++ src/awsSimpleName.tf
@@ -4,18 +4,15 @@
 variable "access_key" {
   type = string
   description = "access key"
-  default = ""
 }
 variable "secret_key" {
   type = string
   description = "secret key"
-  default = ""
   sensitive = true
 }
 variable "region" {
   type = string
   description = "region"
-  default = ""
 }
 #################################
 ##			Provider		   ##

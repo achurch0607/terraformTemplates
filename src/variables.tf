287dcfaaf6e68018e94011389cd9b84f2bd2529d
Adam Church <achurch0607@gmail.com>
2025-07-21 10:05:11 -0600

    testing lock.hcl

--- src/variables.tf
+++ src/variables.tf
@@ -1,35 +1,35 @@
 variable "access_key" {
-    type         = string
-    description  = "AWS Accesss Key"
-    sensitive   = true
+  type        = string
+  description = "AWS Accesss Key"
+  sensitive   = true
 }
 variable "secret_key" {
-    type         = string
-    description  = "AWS Secret Key"
-    sensitive   = true
+  type        = string
+  description = "AWS Secret Key"
+  sensitive   = true
 }
 variable "region" {
-    type         = string
-    description  = "AWS Region"
-    default = "us-east-1"
+  type        = string
+  description = "AWS Region"
+  default     = "us-east-1"
 }
 
 variable "listeners" {
   description = "A list of listener configurations with protocol, port, and SSL certificate ARN"
   type = list(object({
-    protocol       = string
-    port           = number
-    ssl_cert_data  = optional(string)
+    protocol      = string
+    port          = number
+    ssl_cert_data = optional(string)
   }))
   default = [
     {
-      protocol    = "HTTP"
-      port        = 80
+      protocol = "HTTP"
+      port     = 80
       # ssl_cert_arn = "arn:aws:acm:us-east-1:123456789012:certificate/abcd1234-abcd-1234-abcd-123456789012"
     },
     {
-      protocol    = "HTTP"
-      port        = 8081
+      protocol = "HTTP"
+      port     = 8081
       # ssl_cert_arn = "arn:aws:acm:us-east-1:123456789012:certificate/efgh5678-efgh-5678-efgh-567890123456"
     }
   ]
